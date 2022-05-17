d7658274e0c2d2014ef1a219cc151d669d2dfa5a
Kleyson Gomes <kleysongomes@moradadapaz.com.br>
2022-05-17 15:00:01 -0300

    fucoes de validacao do link e exibcao conjuta dos arrays

--- lib-markdown/index.js
+++ lib-markdown/index.js
@@ -1,36 +1,28 @@
+const chalk = require('chalk');
 const fs = require('fs');
 
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] })
+  }
+  return arrayResultados.length === 0 ? 'não há links' : arrayResultados;
+}
+
 function trataErro(erro) {
-  throw new Error(erro.code);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(texto)
-  } catch (erro) {
+    return extraiLinks(texto);
+  } catch(erro) {
     trataErro(erro);
   }
-
 }
 
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((texto) => console.log(texto))
-//     .catch((erro) => trataErro(erro))
-// }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//     if (erro) {
-//       trataErro(erro);
-//     }
-//     console.log((texto));
-//   })
-// }
-
-pegaArquivo('./arquivos/texto1.md'); 
+module.exports = pegaArquivo;
